40ec82d5db2637225c504cb82155657028a26209
Esteban Helguero <esteban.helguero@redk.net>
2021-02-07 08:11:26 +0100

    Added state to the card app

--- src/Card.js
+++ src/Card.js
@@ -1,19 +1,42 @@
 import React, {Component} from 'react';
 
 class Card extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            firstName: "DJ Steve",
+            initials: "DJS",
+            info: [
+                { title: "Birthday", text: "Jan 1, 1980"},
+                { title: "Address", text: "123 Fulton St."},
+                { title: "Phone", text: "212-234-5678"},
+                { title: "Email", text: "e.c@gmail.com"}
+            ]
+        };
+    }
     render(){
+        const {
+            firstName,
+            initials,
+            info
+        } = this.state;
+
         return (
         <React.Fragment>
             <section className="card-container">
                 <header>
-                    <span initials="DJS"></span>
-                    <h2>DJ Steve</h2>
+                    <span initials={initials}></span>
+                    <h2>{firstName}</h2>
                 </header>
                 <main>
                     <ul>
-                        <li><span>Birthday</span> Jan 1, 1980</li>
-                        <li><span>Address</span> 123 Fulton St.</li>
-                        <li><span>Phone</span> 212-234-5678</li>
+                        {info.map((row, index) => {
+                            return (
+                            <li key={index}>
+                                <span>{row.title}</span> {row.text ? row.text : "n/a"}
+                            </li>
+                            )
+                        })}
                     </ul>
                 </main>
             </section>
