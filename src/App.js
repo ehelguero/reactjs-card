0d9683f9435940d4a2bd1ee6a2aafe6315528d2b
Esteban Helguero <esteban.helguero@redk.net>
2021-02-13 22:41:43 +0100

    Added react router dom and created a small menu

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import './App.css';
 import Card from './Card';
 import InputEvents from './InputEvents';
@@ -17,6 +18,24 @@ function App() {
       <Card />
       <InputEvents />
       <MovementEvents />
+      <Router>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+
+        <Route exact path="/" render={() => {
+          return <h2>Home Page</h2>
+        }} />
+        <Route path="/about" render={() => {
+          return <h2>About</h2>
+        }} />
+        <Route path="/contact" render={() => {
+          return <h2>Contact</h2>
+        }} />
+
+      </Router>
     </div>
   );
 }
